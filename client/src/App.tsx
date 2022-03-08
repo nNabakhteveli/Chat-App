16740bf52f4f7458e66bec401ea3153c4bda6a37
Nika Nabakhteveli <nabakhtevelin@gmail.com>
2022-03-09 03:20:22 +0400

    Fixed incoming messages display issue

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,16 +6,27 @@ import './App.css';
 
 const client = new W3CWebSocket('ws://127.0.0.1:8000')
 
-
 interface MessageInterface {
+  type?: string,
   userWhoSent: string,
   msg: string
 }
 
+const ChatContainer = (props: { messagesData: MessageInterface[] }) => {
+  return(
+    <div className='chatContainer'>
+        {
+          props.messagesData.length > 0 ? props.messagesData.map(({ userWhoSent, msg }: MessageInterface) => <h3 key={nanoid()}>{userWhoSent}: {msg}</h3>) : null
+        } 
+    </div>
+  );
+}
+
 function App() {
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState<MessageInterface[]>([]);
   const [username, setUsername] = useState('');
   const [inputValue, setInputValue] = useState('');
+
 
   useEffect(() => {
     client.onopen = () => {
@@ -24,35 +35,44 @@ function App() {
 
     client.onmessage = (msg: any) => {
       const dataFromServer = JSON.parse(msg.data);
-      let arr: any = [...messages, dataFromServer];
-      setMessages(arr);
+      console.log(2, dataFromServer);
+
+      const obj: MessageInterface = {
+        type: dataFromServer.type,
+        userWhoSent: dataFromServer.userWhoSent,
+        msg: dataFromServer.msg
+      };
+
+      if (dataFromServer.recentMessages.length === 0) {
+        const arr: Array<MessageInterface> = messages.concat(obj);
+        setMessages(arr);
+      } else {
+        const arr: Array<MessageInterface> = messages.concat(...dataFromServer.recentMessages, obj);
+        setMessages(arr)
+      }
     }
 
     const temporaryUsername = prompt('Enter your username: ');
 
     setUsername(typeof temporaryUsername === 'string' ? temporaryUsername : '');
   }, []);
-
+  
   const sendMessage = (value: string): void => {
     client.send(JSON.stringify({
+      recentMessages: messages,
       type: "message",
       userWhoSent: username,
       msg: value
     }));
   }
-
+  
   const getValueFromInputField = (event: any) => {
     setInputValue(event.target.value);
   }
-
+  
   return (
     <div className="App">
-      <h1>Hello world!</h1>
-      <div className='chatContainer'>
-        {
-          messages.length > 0 ? messages.map(({ userWhoSent, msg }: MessageInterface)=> <h3 key={nanoid()}>{userWhoSent}: {msg}</h3>) : null
-        } 
-      </div>
+      <ChatContainer messagesData={messages} />
       <input type='text' className="messageInput" onChange={(event) => getValueFromInputField(event)} />
       <br />
       <button onClick={() => sendMessage(inputValue)}>Send message</button>
