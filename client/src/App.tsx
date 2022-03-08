b81be437fcb13a526c855e6361f12324292f02e8
Nika Nabakhteveli <nabakhtevelin@gmail.com>
2022-03-09 01:29:33 +0400

    Added simple markup for chat

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,40 +1,63 @@
 import React, { useState, useEffect } from 'react';
 import { w3cwebsocket as W3CWebSocket } from 'websocket';
+import { nanoid } from 'nanoid';
 import './App.css';
+
 
 const client = new W3CWebSocket('ws://127.0.0.1:8000')
 
 
+interface MessageInterface {
+  userWhoSent: string,
+  msg: string
+}
+
 function App() {
-  const [messages, setMessages] = useState('');
+  const [messages, setMessages] = useState([]);
   const [username, setUsername] = useState('');
+  const [inputValue, setInputValue] = useState('');
 
   useEffect(() => {
-
     client.onopen = () => {
       console.log("Connected to WebSocket successfuly")
     }
 
     client.onmessage = (msg: any) => {
       const dataFromServer = JSON.parse(msg.data);
-      console.log(dataFromServer)
+      let arr: any = [...messages, dataFromServer];
+      setMessages(arr);
     }
+
+    const temporaryUsername = prompt('Enter your username: ');
+
+    setUsername(typeof temporaryUsername === 'string' ? temporaryUsername : '');
   }, []);
 
   const sendMessage = (value: string): void => {
     client.send(JSON.stringify({
       type: "message",
+      userWhoSent: username,
       msg: value
     }));
+  }
+
+  const getValueFromInputField = (event: any) => {
+    setInputValue(event.target.value);
   }
 
   return (
     <div className="App">
       <h1>Hello world!</h1>
-      <button onClick={() => sendMessage("Hello from React!")}>Send message</button>
+      <div className='chatContainer'>
+        {
+          messages.length > 0 ? messages.map(({ userWhoSent, msg }: MessageInterface)=> <h3 key={nanoid()}>{userWhoSent}: {msg}</h3>) : null
+        } 
+      </div>
+      <input type='text' className="messageInput" onChange={(event) => getValueFromInputField(event)} />
+      <br />
+      <button onClick={() => sendMessage(inputValue)}>Send message</button>
     </div>
   );
 }
 
 export default App;
-
